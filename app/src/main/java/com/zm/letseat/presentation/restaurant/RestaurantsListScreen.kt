54287b6937aee74655aea68830eeb6c99191e903
Zeinab Abdelmawla <zeinabmohamed22@gmail.com>
2022-08-23 13:44:24 +0200

    build Composable RestaurantsListScreen

--- app/src/main/java/com/zm/letseat/presentation/restaurant/RestaurantsListScreen.kt
+++ app/src/main/java/com/zm/letseat/presentation/restaurant/RestaurantsListScreen.kt
@@ -1,30 +1,22 @@
 package com.zm.letseat.presentation.restaurant
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalProvider
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,11 +26,44 @@ import com.zm.letseat.domain.restaurant.entity.RestaurantStatus
 import com.zm.letseat.presentation.restaurant.model.RestaurantUi
 
 
-@Preview
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2, heightDp = 300)
+@Composable
+private fun RestaurantsListScreen() {
+    val modifier = Modifier
+    val sortingOption = RestaurantSortOption.RATING
+    val restaurantList = listOf(
+        RestaurantUi(name = "Tandoori Express",
+            status = RestaurantStatus.CLOSED.toString(),
+            sortingValue = "22"),
+        RestaurantUi(name = "Tandoori Express2",
+            status = RestaurantStatus.CLOSED.toString(),
+            sortingValue = "22"),
+        RestaurantUi(name = "Tandoori Express2",
+            status = RestaurantStatus.CLOSED.toString(),
+            sortingValue = "22"),
+        RestaurantUi(name = "Tandoori Express2",
+            status = RestaurantStatus.CLOSED.toString(),
+            sortingValue = "22"),
+        RestaurantUi(name = "Tandoori Express2",
+            status = RestaurantStatus.CLOSED.toString(),
+            sortingValue = "22"), RestaurantUi(name = "Tandoori Express2",
+            status = RestaurantStatus.CLOSED.toString(),
+            sortingValue = "22")
+    )
+    Scaffold(
+        topBar = {
+            RestaurantsListTopAppBar(modifier, sortingOption)
+        },
+        backgroundColor = MaterialTheme.colors.surface
+    ) { padding ->
+        RestaurantsList(modifier.padding(padding), sortingOption, restaurantList)
+    }
+}
+
 @Composable
 private fun RestaurantsListTopAppBar(
     modifier: Modifier = Modifier,
-    sortingOption: RestaurantSortOption = RestaurantSortOption.RATING,
+    sortingOption: RestaurantSortOption,
 ) {
     TopAppBar(
         title = {
@@ -52,21 +77,13 @@ private fun RestaurantsListTopAppBar(
     )
 }
 
-@Preview
 @Composable
 private fun RestaurantsList(
     modifier: Modifier = Modifier,
-    sortingOption: RestaurantSortOption = RestaurantSortOption.RATING,
-    restaurantList: List<RestaurantUi> = listOf(
-        RestaurantUi(name = "Tandoori Express",
-            status = RestaurantStatus.CLOSED.toString(),
-            sortingValue = "22"),
-        RestaurantUi(name = "Tandoori Express2",
-            status = RestaurantStatus.CLOSED.toString(),
-            sortingValue = "22")
-    ),
+    sortingOption: RestaurantSortOption,
+    restaurantList: List<RestaurantUi>,
 ) {
-    LazyColumn {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
         items(restaurantList) { restaurantItem ->
             RestaurantItem(
                 modifier = modifier,
@@ -89,7 +106,7 @@ private fun RestaurantItem(
     sortingOption: RestaurantSortOption = RestaurantSortOption.RATING,
     restaurantUi: RestaurantUi,
 ) {
-    Card(modifier = modifier.padding(8.dp)) {
+    Card(modifier = modifier.padding(8.dp), elevation = 8.dp) {
         Column(modifier
             .fillMaxWidth()
             .padding(8.dp)) {
