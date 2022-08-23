3fec649fcd1ed85d62ccb07916e8eb1cf15a0ac1
Zeinab Abdelmawla <zeinabmohamed22@gmail.com>
2022-08-23 12:49:30 +0200

    build Composable RestaurantsListTopAppBar

--- app/src/main/java/com/zm/letseat/presentation/restaurant/RestaurantsListScreen.kt
+++ app/src/main/java/com/zm/letseat/presentation/restaurant/RestaurantsListScreen.kt
@@ -1,23 +1,56 @@
 package com.zm.letseat.presentation.restaurant
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalProvider
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.zm.letseat.R
 import com.zm.letseat.domain.restaurant.entity.RestaurantSortOption
 import com.zm.letseat.domain.restaurant.entity.RestaurantStatus
 import com.zm.letseat.presentation.restaurant.model.RestaurantUi
+
+
+@Preview
+@Composable
+private fun RestaurantsListTopAppBar(
+    modifier: Modifier = Modifier,
+    sortingOption: RestaurantSortOption = RestaurantSortOption.RATING,
+) {
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier.fillMaxWidth(1f)) {
+                Text(text = stringResource(R.string.restaurants_list))
+            }
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = Color.White,
+    )
+}
 
 @Preview
 @Composable
